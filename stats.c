01b45cca174404b20ab4469cf960240a7e3c58c7
MoHaMeDAboelgoud <mohamed_adel2010202000@yahoo.com>
2018-09-28 21:20:49 +0200

    Adding outline and documentation for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -13,5 +13,45 @@
 
 main()
 {
-	
+	unsigned int inp[] = {1,4,5,6,9,10,14,52,8,41, 
+						56,91,78,11,25,74,65,16,23,78,
+						54,18,19,24,28,27,29,35,36,38
+						41,45,48,49,75,79,82,83,99,98}
+
+
+}
+
+int find_maximum(unsigned int num[] ,int len)
+{
+
+}
+
+int find_minimum(unsigned int num[] ,int len)
+{
+
+}
+
+int find_mean(unsigned int num[] ,int len)
+{
+
+}
+
+int find_median(unsigned int num[] ,int len)
+{
+
+}
+
+int * sort_array(unsigned int num[] ,int len)
+{
+
+}
+
+void print_array(unsigned int num[] ,int len)
+{
+
+}
+
+void print_statistics(unsigned int min,unsigned int max,unsigned int mean,unsigned int median)
+{
+
 }
--- stats.h
+++ stats.h
@@ -1,6 +1,43 @@
 /**
-*@ Maximum function take two numbers
+*@ Maximum function takes the array elements and length
 *and return with the maximum
 *
 */
-unsigned int find_maximum(unsigned int x, unsigned int y);
+int find_maximum(unsigned int num[] ,int len);
+
+/*
+*@Minimum Function takes the array elements and length
+*and return with the minimum
+* 
+*/
+int find_minimum(unsigned int num[] ,int len);
+
+/*
+*@Mean Function takes the array elements and length
+*Returns with the mean valu
+*/
+int find_mean(unsigned int num[] ,int len);
+
+/*
+*@Median Function takes the array elements and length
+*Returns with the median value
+*/
+int find_median(unsigned int num[] ,int len);
+
+/*
+@Sort function takes the array elements and length
+*returns with the array sorts from largest to smallest
+*/
+int * sort_array(unsigned int num[] ,int len);
+
+/*
+@Print array function takes the array elements and length
+*has no return just print array on screen
+*/
+void print_array(unsigned int num[] ,int len);
+
+/*
+*@Print statistics function takes minimum, maximum, mean, and median
+*and print them on the screen
+*/
+void print_statistics(unsigned int min,unsigned int max,unsigned int mean,unsigned int median);
